f791ce9854c5e257f27c9055c1876a66ae131237
mason <mason.rivera1@gmail.com>
2020-05-13 16:55:29 -0500

    firstone
    firstone

---
+++ myfirstproject/src/myfirstproject/mainclass.java
@@ -0,0 +1,10 @@
+package myfirstproject;
+
+public class mainclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hellow world");
+	}
+
+}
